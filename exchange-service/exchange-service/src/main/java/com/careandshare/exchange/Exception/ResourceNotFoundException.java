a0183bde4b59746060e35c606815f4ba56774d44
Vijay Thangaraj <vijaythangarahj@outlook.com>
2025-10-16 00:50:11 +0530

    Delete exchange-service/exchange-service directory

--- exchange-service/exchange-service/src/main/java/com/careandshare/exchange/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.careandshare.exchange.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
